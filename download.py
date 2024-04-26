16fae8dc8794fd5de119a514d424e8c7405b68ca
Philpax <me@philpax.me>
2024-04-27 01:43:50 +0200

    feat: support downloading ondemand sessions

--- download.py
+++ download.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import List, Optional
 from tqdm import tqdm
 from pydantic import BaseModel
+from bs4 import BeautifulSoup
 
 import requests
 import argparse
@@ -13,8 +14,10 @@ import json
 
 RAINFOCUS_WIDGET_ID = "C5aHR3OlA60pUDILVE33Jbn8hagS4Fsw"
 RAINFOCUS_API_PROFILE_ID = "hUVpYtzXsLcOoh4rkNjxpSodRKdtJlUs"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
 
 BASE_HEADERS = {
+    "User-Agent": USER_AGENT,
     "Accept": "*/*",
     "Referer": "https://www.nvidia.com/",
     "Origin": "https://www.nvidia.com",
@@ -40,11 +43,15 @@ class Session(BaseModel):
     session_id: str
     title: str
     time: str
-    session_time_id: str
     length: float
     abstract: str
     participants: List[Participant]
     attributes: dict[str, str]
+
+    # Present on Rainforest sessions
+    session_time_id: Optional[str]
+    # Present on on-demand sessions
+    kaltura_id: Optional[str]
 
     def fetch_from_conference(auth_token: str, id: str) -> Session:
         url = "https://events.rainfocus.com/api/session"
@@ -64,7 +71,6 @@ class Session(BaseModel):
             session_id=item["sessionID"],
             title=item["title"].strip(),
             time=time["utcStartTime"],
-            session_time_id=time["sessionTimeID"],
             length=item["length"],
             abstract=item["abstract"],
             participants=[
@@ -72,6 +78,34 @@ class Session(BaseModel):
                 for p in item["participants"]
             ],
             attributes={a["attribute"]: a["value"] for a in item["attributevalues"]},
+            session_time_id=time["sessionTimeID"],
+            kaltura_id=None,
+        )
+
+    def fetch_from_ondemand(id: str) -> Session:
+        url = f"https://api-prod.nvidia.com/services/nod/api/v1/session?id={id}"
+
+        response = requests.get(url, headers=BASE_HEADERS)
+        json = response.json()
+
+        data = json["sessionData"]
+
+        return Session(
+            session_id=data["sessionID"],
+            title=data["sTitle"].strip(),
+            time=data["sDate"],
+            length=data["sLength"],
+            abstract=data["sAbstract"],
+            participants=[
+                {
+                    "name": f"{p.get('firstName')} {p.get('lastName')}",
+                    "bio": p.get("bio"),
+                }
+                for p in data["speakerList"]
+            ],
+            attributes={a["attribute"]: a["value"] for a in data["attributes"]},
+            session_time_id=None,
+            kaltura_id=data["assets"][0]["url"].split(":")[-1],
         )
 
 
@@ -117,41 +151,56 @@ class KalturaMetadata(BaseModel):
     entry_id: str
     profiles: List[FlavorProfile]
 
-    def fetch(webinar: Webinar) -> KalturaMetadata:
+    def fetch_from_params(
+        partner_id: str, entry_id: str, ks: Optional[str]
+    ) -> KalturaMetadata:
         url = "https://cdnapisec.kaltura.com/api_v3/service/multirequest"
-
         headers = {**BASE_HEADERS, "Content-Type": "application/json"}
 
+        # Set up the base request
         request = {
-            "1": {
-                "service": "baseEntry",
-                "action": "list",
-                "ks": webinar.ks,
-                "filter": {"redirectFromEntryId": webinar.entry_id},
-                "responseProfile": {"type": 1, "fields": "id"},
-            },
-            "2": {
-                "service": "baseEntry",
-                "action": "getPlaybackContext",
-                "entryId": "{1:result:objects:0:id}",
-                "ks": webinar.ks,
-                "contextDataParams": {
-                    "objectType": "KalturaContextDataParams",
-                    "flavorTags": "all",
-                },
-            },
             "apiVersion": "3.3.0",
             "format": 1,
-            "ks": webinar.ks,
+            "ks": ks if ks else "",
             "clientTag": "html5:v3.17.10",
-            "partnerId": webinar.partner_id,
+            "partnerId": partner_id,
+        }
+
+        # Add a first step to start a widget session if ks is not provided
+        # and use its result for ks in the next steps
+        first_step = 1 if ks is not None else 2
+        second_step = first_step + 1
+        if ks is None:
+            request["1"] = {
+                "action": "startWidgetSession",
+                "service": "session",
+                "widgetId": f"_{partner_id}",
+            }
+            ks = "{1:result:ks}"
+
+        request[str(first_step)] = {
+            "service": "baseEntry",
+            "action": "list",
+            "ks": ks,
+            "filter": {"redirectFromEntryId": entry_id},
+            "responseProfile": {"type": 1, "fields": "id"},
+        }
+        request[str(second_step)] = {
+            "service": "baseEntry",
+            "action": "getPlaybackContext",
+            "entryId": f"{{{first_step}:result:objects:0:id}}",
+            "ks": ks,
+            "contextDataParams": {
+                "objectType": "KalturaContextDataParams",
+                "flavorTags": "all",
+            },
         }
 
         response = requests.post(url, headers=headers, json=request)
         output = response.json()
 
-        entry_id = output[0]["objects"][0]["id"]
-        profiles = output[1]["flavorAssets"]
+        entry_id = output[first_step - 1]["objects"][0]["id"]
+        profiles = output[second_step - 1]["flavorAssets"]
         return KalturaMetadata(
             entry_id=entry_id,
             profiles=[
@@ -166,34 +215,15 @@ class KalturaMetadata(BaseModel):
             ],
         )
 
-    def fetch_length(self, webinar: Webinar, profile: FlavorProfile) -> float:
-        url = self._manifest_url(webinar, profile)
-        command = [
-            "ffprobe",
-            "-v",
-            "error",
-            "-show_entries",
-            "format=duration",
-            "-of",
-            "default=noprint_wrappers=1:nokey=1",
-        ]
-        command.extend(self._headers_args())
-        command.append(url)
-
-        result = subprocess.run(
-            command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
-        )
-        duration = float(result.stdout)
-        return duration
-
     def fetch_video(
         self,
-        webinar: Webinar,
+        partner_id: str,
+        ks: Optional[str],
         profile: FlavorProfile,
         duration: float,
         output_path: str,
     ):
-        url = self._manifest_url(webinar, profile)
+        url = self._manifest_url(partner_id, ks, profile)
         command = [
             "ffmpeg",
             "-loglevel",
@@ -251,8 +281,14 @@ class KalturaMetadata(BaseModel):
             output.append(f"{kv[0]}: {kv[1]}")
         return output
 
-    def _manifest_url(self, webinar: Webinar, profile: FlavorProfile) -> str:
-        return f"https://cdnapisec.kaltura.com/p/{webinar.partner_id}/sp/{webinar.partner_id}00/playManifest/entryId/{self.entry_id}/protocol/https/format/applehttp/flavorIds/{profile.id}/ks/{webinar.ks}/a.m3u8"
+    def _manifest_url(
+        self, partner_id: str, ks: Optional[str], profile: FlavorProfile
+    ) -> str:
+        output = f"https://cdnapisec.kaltura.com/p/{partner_id}/sp/{partner_id}00/playManifest/entryId/{self.entry_id}/protocol/https/format/applehttp/flavorIds/{profile.id}"
+        if ks is not None:
+            output += f"/ks/{ks}"
+        output += "/a.m3u8"
+        return output
 
 
 class OutputMetadata(BaseModel):
@@ -298,7 +334,9 @@ if __name__ == "__main__":
         description="Download a video from the NVIDIA GTC event"
     )
     parser.add_argument(
-        "-a", "--rainforest-auth", help="The Rainforest auth token", required=True
+        "-a",
+        "--rainforest-auth",
+        help="The Rainforest auth token, required only for conference sessions",
     )
     parser.add_argument(
         "-d", "--directory", help="The directory to save the video to", default=""
@@ -309,11 +347,42 @@ if __name__ == "__main__":
         help="Write metadata to a file",
         action="store_true",
     )
-    parser.add_argument("id", help="The ID of the video to download")
+    parser.add_argument(
+        "-c",
+        "--conf-session-id",
+        help="The ID of the video to download from the conference",
+    )
+    parser.add_argument(
+        "-o",
+        "--ondemand-session-url",
+        help="The URL of the video to download from on-demand",
+    )
     args = parser.parse_args()
 
-    # --------------------------------------------
-    session = Session.fetch_from_conference(args.rainforest_auth, args.id)
+    # Get the session from the provided ID or URL
+    partner_id = None
+
+    if args.conf_session_id:
+        session = Session.fetch_from_conference(
+            args.rainforest_auth, args.conf_session_id
+        )
+    elif args.ondemand_session_url:
+        response = requests.get(args.ondemand_session_url)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        # get partner id from image url
+        image = soup.find("meta", attrs={"property": "og:image"}).get("content")
+        partner_id = image.split("/")[4]
+        session_id = soup.find("meta", attrs={"property": "event_sessionId"}).get(
+            "content"
+        )
+
+        session = Session.fetch_from_ondemand(session_id)
+
+    else:
+        raise ValueError("No session ID provided")
+
+    # Prepare the paths
     print(f"title: {session.title}")
     print(f"abstract: {session.abstract}")
     print(f"presenters: {', '.join(p.name for p in session.participants)}")
@@ -334,32 +403,40 @@ if __name__ == "__main__":
             os.remove(meta_path)
         print(f"metadata path: {meta_path}")
 
-    # --------------------------------------------
-    webinar = Webinar.fetch(args.rainforest_auth, session)
+    # Get the metadata and select our profile
+    entry_id = None
+    ks = None
+    if args.conf_session_id:
+        webinar = Webinar.fetch(args.rainforest_auth, session)
+        partner_id = webinar.partner_id
+        entry_id = webinar.entry_id
+        ks = webinar.ks
+    else:
+        entry_id = session.kaltura_id
 
-    # --------------------------------------------
-    metadata = KalturaMetadata.fetch(webinar)
+    metadata = KalturaMetadata.fetch_from_params(partner_id, entry_id, ks)
+
     profile = max(
         metadata.profiles, key=lambda p: p.width * p.height * p.bitrate * p.frame_rate
     )
     print("selected profile:", profile)
 
-    # --------------------------------------------
-    length = metadata.fetch_length(webinar, profile)
-    print(f"length: {length} seconds")
-
-    # --------------------------------------------
-    return_code = metadata.fetch_video(webinar, profile, length, video_path)
+    # Download the video
+    return_code = metadata.fetch_video(
+        partner_id, ks, profile, session.length, video_path
+    )
     if return_code == 0:
         print("download: succeeded")
     else:
         print("download: failed")
 
-    # --------------------------------------------
+    # Write metadata
     if args.meta:
         with open(meta_path, "w") as f:
             output_metadata = OutputMetadata.build(session, profile)
             output_metadata_dict = output_metadata.dict()
             f.write(json.dumps(output_metadata_dict, indent=4))
         print("metadata: written")
+
+    # Exit
     exit(return_code)
